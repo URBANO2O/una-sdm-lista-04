00c43129d96f4ccea38775ab01ca7211f0486893
URBANO2O <urbanoneto37@gmail.com>
2026-03-01 20:18:26 -0300

    Add files via upload

---
+++ AdviceResponse.cs
@@ -0,0 +1,10 @@
+public class AdviceResponse
+{
+    public Slip slip { get; set; }
+}
+
+public class Slip
+{
+    public int id { get; set; }
+    public string advice { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Iniciando requisição para obter dados de um conselho...");
+        
+        string url = "https://api.adviceslip.com/advice";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                var adviceResponse = JsonSerializer.Deserialize<AdviceResponse>(json);
+
+                Console.WriteLine("\nConselho de Hoje:");
+                Console.WriteLine(adviceResponse.slip.advice);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao consumir API:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        Console.WriteLine("\nFim da requisição.");
+    }
+}
